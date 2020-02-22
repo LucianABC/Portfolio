b64449b21015c0546f680bfac5d1e24da34e0f81
LucianABC <louabecasis@gmail.com>
2020-02-22 12:41:57 -0300

    contact me

--- src/sections/Contact/Contact.js
+++ src/sections/Contact/Contact.js
@@ -4,7 +4,12 @@ import '../Section.scss';
 const Contact = ()=>{
     return(
         <section className="contact section">
-
+            <div className="cellphone">
+                <div className="cellphone-item">Celular</div>
+                <div className="cellphone-item">Email</div>
+                <div className="cellphone-item">LinkedIn</div>
+                <div className="cellphone-item">Blah</div>
+            </div>
         </section>
     ); 
 }
